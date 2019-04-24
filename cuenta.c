700d1cb65d88cb6cd67041878be4ca2eea7375e4
Cornmaster9876 <cportilla2589@gmail.com>
2019-04-24 15:16:57 -0400

    Initial travis release

--- cuenta.c
+++ cuenta.c
@@ -1,3 +1,9 @@
+#include "defs.h"
+#include "stdlib.h"
+#include "proc.h"
+#include "syscall.h"
+
 int main(void){
-    printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
+    printf(1, "La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
+exit();
 }
