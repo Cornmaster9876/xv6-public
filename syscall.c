c705b1327ee591e10dbd40f75ebe55339d9a4057
Cornmaster9876 <cportilla2589@gmail.com>
2019-06-19 01:42:31 -0400

    Virtual Address to Physical Adress

--- syscall.c
+++ syscall.c
@@ -129,7 +129,7 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_getprocs]   sys_getprocs,
-[SYS_physAdress]   sys_physAddress,
+[SYS_physAddress]   sys_physAddress,
 };
 
 void
